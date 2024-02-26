c2d8fa361f7a92a57f1ee4558ab5abece1ceadb2
Misiryung <132610839+Misiryung@users.noreply.github.com>
2024-02-26 19:42:10 +0800

    API.ts

--- src/API.ts
+++ src/API.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "YOUR_API_KEY";
+const API_KEY = "AIzaSyDHwIthpdcAEHl8v-B-xe0TXsk8lwugRuE";
 
 export const searchVideos = async (query: string) => {
   try {
