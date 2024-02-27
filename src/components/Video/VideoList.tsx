5b110925611126e4d289d79e232eec3b4f87bd63
Misiryung <132610839+Misiryung@users.noreply.github.com>
2024-02-27 15:42:21 +0800

    VideoList.tsx

--- src/components/Video/VideoList.tsx
+++ src/components/Video/VideoList.tsx
@@ -4,9 +4,16 @@ import Loader from "../Loader";
 import VideoCard from "./VideoCard";
 import { VideoTypes } from "./VideoTypes";
 
-const VideoList: React.FC<VideoListProps> = ({ videoList, direction }) => {
+const VideoList: React.FC<VideoListProps> = ({
+  videoList,
+  direction,
+  maxCards,
+}) => {
   // 如果视频列表为空，显示加载器
   if (!videoList?.length) return <Loader />;
+
+  // 如果提供了最大卡片数量，则对视频列表进行截取
+  const slicedVideoList = maxCards ? videoList.slice(0, maxCards) : videoList;
 
   return (
     // 使用 Stack 组件包裹视频卡片列表，支持灵活布局
@@ -16,7 +23,7 @@ const VideoList: React.FC<VideoListProps> = ({ videoList, direction }) => {
       justifyContent="center"
       marginTop="10px"
     >
-      {videoList.map((video, index) => (
+      {slicedVideoList.map((video, index) => (
         // 使用 Box 组件包裹每个视频卡片
         <Box key={index}>
           {/* 如果视频的唯一标识符存在，则渲染视频卡片 */}
@@ -31,6 +38,7 @@ const VideoList: React.FC<VideoListProps> = ({ videoList, direction }) => {
 interface VideoListProps {
   videoList: VideoTypes[]; // 视频列表数据
   direction?: "row" | "column"; // 视频列表方向，默认为行
+  maxCards?: number; // 最大卡片数量
 }
 
 export default VideoList;
