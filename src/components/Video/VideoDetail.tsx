edf2ccee681755380a827bc755fbbc9dbfc1bad8
Misiryung <132610839+Misiryung@users.noreply.github.com>
2024-02-28 10:58:43 +0800

    VideoDetail.tsx

--- src/components/Video/VideoDetail.tsx
+++ src/components/Video/VideoDetail.tsx
@@ -2,8 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import ReactPlayer from "react-player";
 import { Typography, Box, Stack } from "@mui/material";
-import DetailIcon from "./icons";
-import { fetchVideoInfo } from "../../API"; // 导入从 API 中获取视频和频道信息的函数
+import { fetchVideoInfo } from "../../API";
 import Loader from "../Loader";
 
 const VideoDetail: React.FC = () => {
@@ -12,7 +11,7 @@ const VideoDetail: React.FC = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      if (!id) return; // 如果 id 为空，则直接返回
+      if (!id) return;
 
       try {
         const videoDetailData = await fetchVideoInfo(id);
@@ -32,15 +31,31 @@ const VideoDetail: React.FC = () => {
   } = videoDetail;
 
   return (
-    <Box minHeight="92vh">
+    <Box height="92vh">
       <Stack direction={{ xs: "column", md: "row" }}>
-        <Box flex={1} width={288}>
-          <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
-            <ReactPlayer
-              url={`https://www.youtube.com/watch?v=${id}`}
-              className="react-player"
-              controls
-            />
+        <Box flex={1}>
+          <Box
+            sx={{
+              width: "calc(100% - 20px)",
+              margin: "auto",
+              marginTop: "10px",
+              paddingLeft: "20px",
+            }}
+          >
+            <Box
+              sx={{
+                borderRadius: "10px", // 添加圆角属性
+                overflow: "hidden", // 确保溢出部分被裁剪为圆角
+              }}
+            >
+              <ReactPlayer
+                url={`https://www.youtube.com/watch?v=${id}`}
+                className="react-player"
+                controls
+                width="100%" // 设置视频播放器宽度为 100%
+                height="auto" // 让视频播放器的高度根据宽度自动调整，保持长宽比例不变
+              />
+            </Box>
             <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
               {title}
             </Typography>
@@ -54,11 +69,20 @@ const VideoDetail: React.FC = () => {
               <Link to={`/channel/${channelId}`}>
                 <Typography variant="subtitle1" color="#fff">
                   {channelTitle}
-                  <DetailIcon fill="#7F7F7F" width={24} height={24} />
                 </Typography>
               </Link>
             </Stack>
           </Box>
+        </Box>
+        <Box
+          px={2}
+          py={{ md: 1, xs: 5 }}
+          height="92vh"
+          justifyContent="center"
+          alignItems="center"
+          sx={{ width: "320px", marginLeft: "auto" }}
+        >
+          <Loader />
         </Box>
       </Stack>
     </Box>
